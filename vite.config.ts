9310de437aee852eeb55aa2a3f1c18583ad47a81
Thomas Mol <email@thomasmol.com>
2026-08-21 16:29:10 +0200

    Switch SvelteKit from static to node adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
