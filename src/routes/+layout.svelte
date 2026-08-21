d3c51dac141e82bec1fa05a9605a40cda0367c5b
Thomas Mol <email@thomasmol.com>
2026-08-21 15:44:32 +0200

    Rebuild project with sv SvelteKit v5 template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import '../app.css';
+	import './layout.css';
+	import favicon from '#lib/assets/favicon.svg';
 
+	let { children } = $props();
 </script>
-<svelte:head>
-	<script defer data-domain="thomasmol.com" src="https://plausible.io/js/script.js"></script>
-	<meta property="og:url" content="{$page.url.toString()}" />
-	<meta name="twitter:url" content="{$page.url.toString()}" />
-	<meta name="linkedin:url" content="{$page.url.toString()}" />
-	<meta name="canonical" content="{$page.url.toString()}" />
-</svelte:head>
 
-<slot />
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
+import { mdsvex } from 'mdsvex';
+import tailwindcss from '@tailwindcss/vite';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+				experimental: { async: true }
+			},
+			adapter: adapter(),
+			preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
+			extensions: ['.svelte', '.svx', '.md'],
+			experimental: { remoteFunctions: true }
+		})
+	]
+});
